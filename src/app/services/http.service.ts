8ba116f6749232571512b16b5b9225d69e21940c
SpringIntoJava <springintojava@gmail.com>
2025-08-17 15:40:06 +0300

    update baseurl for prod

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -6,7 +6,9 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class HttpService {
-  private baseUrl = 'http://localhost:8080/member'; // Replace with your backend URL
+  //private baseUrl = 'http://localhost:8080/shul/member'; // local
+  //private baseUrl = 'https://ec2-13-60-6-160.eu-north-1.compute.amazonaws.com/shul/member'; // production
+  private baseUrl = 'https://api.elcmembers.online/shul/member'; // production
 
   constructor(private http: HttpClient) {}
 
